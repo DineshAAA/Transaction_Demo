4f081c3dc2a7f8438fee067d02e3b57613da3b89
DineshAAA <112423368+DineshAAA@users.noreply.github.com>
2022-09-07 13:12:06 +0530

    Delete src/main directory

--- src/main/java/net/springboot/Transaction/Repository/TransactionDataRepository.java
+++
@@ -1,9 +0,0 @@
-package net.springboot.Transaction.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.springboot.Transaction.Entity.TransactionData;
-
-public interface TransactionDataRepository extends JpaRepository<TransactionData, Long> {
-
-}
